a25ce048266ab0820c063b05513fa126a3aae97f
Thiago Menezes de Oliveira <thiago.mhx@gmail.com>
2025-09-22 19:08:53 -0400

    update pdf

--- apps/site/app/layout.tsx
+++ apps/site/app/layout.tsx
@@ -2,16 +2,25 @@ import { ThemeProvider } from '@t8pro/design-system';
 import type { Metadata } from 'next';
 import { Geist_Mono, Montserrat } from 'next/font/google';
 import '@t8pro/design-system/styles';
+import localFont from 'next/font/local';
 import { TargetCursor } from '@/components/gsap/target-cursor';
 
-const geistSans = Montserrat({
+const montserrat = Montserrat({
   variable: '--font-montserrat',
   subsets: ['latin'],
 });
 
-const geistMono = Geist_Mono({
+const jetBrainsMono = Geist_Mono({
   variable: '--jet-brains-mono',
   subsets: ['latin'],
+});
+
+const materialSymbols = localFont({
+  src: './fonts/material-symbols-rounded.woff2',
+  variable: '--font-material-symbols',
+  display: 'swap',
+  preload: true,
+  adjustFontFallback: false,
 });
 
 export const metadata: Metadata = {
@@ -27,7 +36,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body
+        className={`${montserrat.variable} ${jetBrainsMono.variable} ${materialSymbols.variable}`}
+        style={{ backgroundColor: 'var(--color-secondary-invert)' }}
+      >
         <TargetCursor spinDuration={2} hideDefaultCursor={true} />
         <ThemeProvider>{children}</ThemeProvider>
       </body>
